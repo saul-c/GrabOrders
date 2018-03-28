b06a4dca4af4834304d938f8aa33632676a2aad8
jacklightChen <1797079433@qq.com>
2018-03-28 11:56:33 +0800

    3.28init

--- src/main/java/cn/lightina/GrabOrders/controller/CategoryController.java
+++ src/main/java/cn/lightina/GrabOrders/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package cn.lightina.GrabOrders.controller;
 
-import cn.lightina.GrabOrders.dao.CategoryMapper;
 import cn.lightina.GrabOrders.service.CategoryService;
 import cn.lightina.GrabOrders.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,7 +20,11 @@ public class CategoryController {
     public ModelAndView listCategory(){
         ModelAndView mav = new ModelAndView();
         List<Category> cs= categoryService.list();
-
+        //List<Category>cs=new ArrayList<Category>();
+        Category temp=new Category();
+        temp.setId(1);
+        temp.setName("aaa");
+        cs.add(temp);
         // 放入转发参数
         mav.addObject("cs", cs);
         // 放入jsp路径
